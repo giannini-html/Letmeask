140315fdec96a1db831b17d730f586ffccbdfe9b
Gustavo Giannini <gustavo-giannini@hotmail.com>
2021-06-25 15:54:03 -0300

    Criada primeira versão completa do projeto

---
+++ README.md
@@ -0,0 +1,53 @@
+<h1 align="center">
+    <img src="./src/assets/images/logo.svg">
+</h1>
+
+<h1>
+    <img src="./src/assets/images/letme.gif">
+</h1>
+
+<h1 align="center">
+    <a href="https://letmeask-321bb.web.app/">Acessar a demonstração</a>
+</h1>
+
+# Indice
+- [Sobre](#-sobre)
+- [Tecnologias utilizadas](#-tecnologias-utilizadas)
+- [Como baixar o projeto](#-como-baixar-o-projeto)
+
+## 📝Sobre
+
+O projeto **Letmeask** é uma plataforma de criação de salas online que foi criada dentro do curso da **NLW/Together** para colocar em prática conhecimentos prévios dos alunos.
+
+---
+
+## 💻Tecnologias utilizadas
+
+O projeto foi desenvolvido utilizando as seguintes tecnologias: 
+
+- [ReactJS](https://pt-br.reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+- [Sass](https://sass-lang.com/)
+
+---
+
+## 📂Como baixar o projeto
+
+```bash
+
+    # Clonar o repositório
+    $ git clone https://github.com/giannini-html/Letmeask
+
+    # Entrar no diretório
+    $ cd Letmeask-main
+
+    # Instalar as dependências
+    $ npm install
+
+    #Iniciar o projeto
+    $ npm start
+```
+
+
+---
+Desenvolvido por Gustavo Giannini
---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,121 @@
+import { useHistory, useParams } from 'react-router-dom';
+
+import { RoomCode } from '../components/RoomCode';
+import deleTeImg from '../assets/images/delete.svg';
+import logoImg from '../assets/images/logo.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+import { Button } from '../components/Button';
+import '../styles/room.scss';
+// import { useAuth } from '../hooks/useAuth';
+import { Question } from '../components/Question';
+import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
+
+type RoomParams = {
+    id: string;
+}
+
+export function AdminRoom() {
+    // const { user } = useAuth();
+    const history = useHistory();
+    const params = useParams<RoomParams>();
+    const roomId = params.id;
+
+    const { title, questions } = useRoom(roomId)
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion (questionId: string) {
+        if (window.confirm('Tem certeza que você deseja excluir essa pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
+
+    async function handleQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true,
+        })
+        
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true,
+        })
+        
+    }
+
+    return(
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>
+                        <RoomCode code={roomId} />
+                        <Button 
+                        isOutlined
+                        onClick={handleEndRoom}
+                        >Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <header>Sala {title}</header>
+                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+                </div>
+
+
+                <div className="question-list">
+                {questions.map(question => {
+                    return(
+                        <Question
+                        key={question.id}
+                         content={question.content}
+                         author={question.author}
+                         isAnswered={question.isAnswered}
+                         isHighlighted={question.isHighlighted}
+                        >
+                            
+                            {!question.isAnswered &&  (
+                                <>
+                                    <button
+                                        type="button"
+                                        onClick={() => handleQuestionAsAnswered(question.id)}
+                                    >
+                                        <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                    </button>
+
+                                    <button
+                                        type="button"
+                                        onClick={() => handleHighlightQuestion(question.id)}
+                                    >
+                                        <img src={answerImg} alt="Dar destaque a pergunta" />
+                                    </button>
+                                </>
+                            )}
+
+                            <button
+                                type="button"
+                                onClick={() => handleDeleteQuestion(question.id)}
+                            >
+                                <img src={deleTeImg} alt="Remover pergunta" />
+                            </button>
+                        </Question>
+                        
+                    )
+                })}
+                </div>
+            </main>
+        </div>
+    )
+}
